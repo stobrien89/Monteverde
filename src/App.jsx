e8f02ec22028ac264c3d30261229cba94257d5b6
stobrien89 <stobrien@crimson.ua.edu>
2020-09-20 18:11:36 -0700

    add navbar component, style logo, search bar

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import "./App.css";
+
+import NavBar from "./components/nav/NavBar";
+
+function App() {
+  return (
+    <div className="app">
+      <NavBar></NavBar>
+      <h1>Testing Testing</h1>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/nav/NavBar.jsx
@@ -0,0 +1,35 @@
+import React from "react";
+import "./navbar.css";
+import SearchIcon from "@material-ui/icons/Search";
+
+const NavBar = () => {
+  return (
+    <div className="navbar">
+      <img
+        src="http://pngimg.com/uploads/amazon/amazon_PNG11.png"
+        alt="logo"
+        className="navbar__logo"
+      />
+      <div className="navbar__search">
+        <input type="text" className="navbar__searchInput" />
+        <SearchIcon className="navbar__searchIcon"></SearchIcon>
+      </div>
+      <div className="navbar__nav">
+        <div className="nav__option">
+          <span className="nav__optionLineOne">Hello User</span>
+          <span className="nav__optionLineTwo">Sign In</span>
+        </div>
+        <div className="nav__option">
+          <span className="nav__optionLineOne">Returns</span>
+          <span className="nav__optionLineTwo">& Orders</span>
+        </div>
+        <div className="nav__option">
+          <span className="nav__optionLineOne">Your</span>
+          <span className="nav__optionLineTwo">Prime</span>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default NavBar;
