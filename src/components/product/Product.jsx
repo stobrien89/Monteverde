087eb740a89ea4c90162f8610246e01b67778b28
stobrien89 <stobrien@crimson.ua.edu>
2020-10-06 13:33:03 -0700

    add image, star rating, styling to product component

--- src/components/product/Product.jsx
+++ src/components/product/Product.jsx
@@ -10,7 +10,26 @@ const Product = () => {
           <small>$</small>
           <strong>19.99</strong>
         </p>
+        <div className="product__rating">
+          <img
+            src="https://cms-assets.tutsplus.com/uploads/users/34/posts/30118/preview_image/star-rating.jpg"
+            alt=""
+          />
+          <img
+            src="https://cms-assets.tutsplus.com/uploads/users/34/posts/30118/preview_image/star-rating.jpg"
+            alt=""
+          />
+          <img
+            src="https://cms-assets.tutsplus.com/uploads/users/34/posts/30118/preview_image/star-rating.jpg"
+            alt=""
+          />
+        </div>
       </div>
+      <img
+        src="https://images-na.ssl-images-amazon.com/images/I/81-QB7nDh4L.jpg"
+        alt=""
+      />
+      <button>Add to Basket</button>
     </div>
   );
 };
